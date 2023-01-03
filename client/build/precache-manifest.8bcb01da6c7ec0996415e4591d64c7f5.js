fb6eca5a950e6907215ebc76a8397f4cbb978213
Gulshan2589 <87886553+Gulshan2589@users.noreply.github.com>
2023-01-03 12:56:54 +0530

    removedBuildfromgitignore

---
+++ client/build/precache-manifest.8bcb01da6c7ec0996415e4591d64c7f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "87c8215b5dd969881499",
+    "url": "/static/css/main.a00d12eb.chunk.css"
+  },
+  {
+    "revision": "87c8215b5dd969881499",
+    "url": "/static/js/main.2a0a5fd2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "22b4e60a8343c093e111",
+    "url": "/static/css/2.f22e163b.chunk.css"
+  },
+  {
+    "revision": "22b4e60a8343c093e111",
+    "url": "/static/js/2.34909dd7.chunk.js"
+  },
+  {
+    "revision": "bfdc4a8e798705609ea2bfdd22ea574d",
+    "url": "/index.html"
+  }
+];
